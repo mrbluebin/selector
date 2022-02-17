db8b1193a764ab393155a03a358100d74169583c
박찬빈 <mrbluebin@bagchanbin-ui-MacBookPro.local>
2022-02-17 12:14:15 +0900

    start react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          selector project.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/" //주소
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          버튼
         </a>
       </header>
     </div>
